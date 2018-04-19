6db1ce77eb85eaf92ca1db222393af23e0394d38
Florian Perrotti <florian.perrotti@gmail.com>
2018-04-19 21:46:30 +0200

    chore(init): initialize webhook project

---
+++ README.md
@@ -0,0 +1,4 @@
+# webhook-echo-dialogflow
+
+Based on https://github.com/SunilSyal/Webhook-and-API.AI-Echo-Sample/blob/master/package.json
+Updates for Dialogflow V2. 
---
+++ index.js
@@ -0,0 +1,56 @@
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const restService = express();
+
+restService.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
+
+restService.use(bodyParser.json());
+
+
+restService.get("/", function (req, res) {
+    return res.text('Hello from Echo sample!');
+});
+
+restService.post("/echo", function(req, res) {
+
+    console.log('Incoming request', req);
+
+    var speech =
+        req.body.result &&
+        req.body.result.parameters &&
+        req.body.result.parameters.echoText
+            ? req.body.result.parameters.echoText
+            : "Seems like some problem. Speak again.";
+
+    console.log('speech:', speech);
+
+    return res.json({
+        fulfillmentText: speech,
+        payload: {
+            google: {
+                expectUserResponse: true,
+                richResponse: {
+                    items: [
+                        {
+                            simpleResponse: {
+                                textToSpeech: speech
+                            }
+                        }
+                    ]
+                }
+            }
+        },
+        source: "https://echo-webhook-dialogflow.herokuapp.com"
+    });
+});
+
+restService.listen(process.env.PORT || 8000, function() {
+    console.log("Server up and listening");
+});
