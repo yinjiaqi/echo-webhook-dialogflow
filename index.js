94a6c15b990a8262c116d8691abf4e72e0605899
yinjiaqi <40558566+yinjiaqi@users.noreply.github.com>
2018-07-09 11:17:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -40,9 +40,37 @@ restService.post("/echo", function(req, res) {
                     items: [
                         {
                             simpleResponse: {
-                                textToSpeech: speech
+                                textToSpeech: Math and prime numbers it is!
+                            }
+                        },
+                         {
+                            "basicCard": {
+                                "title": "Math & prime numbers",
+                                "formattedText": "42 is an even composite number. It\n    is composed of three distinct prime numbers multiplied together. It\n    has a total of eight divisors. 42 is an abundant number, because the\n    sum of its proper divisors 54 is greater than itself. To count from\n    1 to 42 would take you about twenty-one…",
+                                "image": {
+                                    "url": "https://example.google.com/42.png",
+                                    "accessibilityText": "Image alternate text"
+                                },
+                                "buttons": [
+                                    {
+                                        "title": "Read more",
+                                        "openUrlAction": {
+                                            "url": "https://example.google.com/mathandprimes"
+                                        }
+                                    }
+                                ],
+                                "imageDisplayOptions": "CROPPED"
                             }
                         }
+                    ],
+                    "suggestions": []
+                }
+            },
+            "possibleIntents": [
+                {
+                    "intent": "actions.intent.text"
+                }
+            ]
                     ]
                 }
             }
