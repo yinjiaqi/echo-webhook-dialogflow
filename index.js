7be468bc4db89110c1e549763104adb287f79589
yinjiaqi <40558566+yinjiaqi@users.noreply.github.com>
2018-07-09 12:29:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ restService.post("/echo", function(req, res) {
                         },
                         {
                             "basicCard": {
-                                "title": Echo V2 card display,
+                                "title": "Echo V2 card display",
                                 "formattedText": "Your Query Input"+speech,
                                 "image": {
                                     "url": "http://image.baidu.com/search/detail?z=0&word=%E6%B2%AA%E4%B8%8A%E5%B0%BE%E7%94%9F&hs=0&pn=2&spn=0&di=0&pi=54979624680&tn=baiduimagedetail&is=0%2C0&ie=utf-8&oe=utf-8&cs=592577143%2C1463899829&os=&simid=&adpicid=0&lpn=0&fm=&sme=&cg=&bdtype=-1&oriquery=&objurl=http%3A%2F%2Fa.hiphotos.baidu.com%2Fimage%2Fpic%2Fitem%2Fb219ebc4b74543a96a58c53112178a82b801148f.jpg&fromurl=&gsm=0&catename=pcindexhot&islist=&querylist=",
