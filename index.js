48af5532176f481bb3547ca308729aa118d91482
yinjiaqi <40558566+yinjiaqi@users.noreply.github.com>
2018-07-09 11:22:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -40,12 +40,12 @@ restService.post("/echo", function(req, res) {
                     items: [
                         {
                             simpleResponse: {
-                                textToSpeech: Math and prime numbers it is!
+                                textToSpeech: speech
                             }
                         },
                          {
                             "basicCard": {
-                                "title": "Math & prime numbers",
+                                "title": speech,
                                 "formattedText": "42 is an even composite number. It\n    is composed of three distinct prime numbers multiplied together. It\n    has a total of eight divisors. 42 is an abundant number, because the\n    sum of its proper divisors 54 is greater than itself. To count from\n    1 to 42 would take you about twenty-one…",
                                 "image": {
                                     "url": "https://example.google.com/42.png",
